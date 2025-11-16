cee4471079e95e7d89b20e8834ba46f563b00fce
laureen <laureen.sinegre@gmail.com>
2025-11-16 15:12:03 +0100

    maj

--- test.c
+++ test.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-
+#include <math.h>
+#include <string.h>
 int main()
 {
     printf("Hello");
     printf("Coucou \n");
+
+    int var = 4;
+    int var_carre = pow(var,2);
+
+
+    char mot_deviner[] = "bonjour";
+    int len = strlen(mot_deviner);
+
+    char joueur ;
+
+    int lettre_presente = 0 ;
+    for(int i = 0 ; i < len ; i++)
+    {
+        printf("%c" , mot_deviner[i]);
+
+        if( joueur == mot_deviner[i])
+        {
+            lettre_presente++;
+        }
+    }
+
+
     return 0;
 }
